d110e35eac1d8169e4514ad673a893a906c8c3ef
jazzzz58rus <56487403+jazzzz58rus@users.noreply.github.com>
2019-10-12 22:05:03 +0300

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO [Products] (Name, Price)VALUES(N'Картошка', 60);
+
+UPDATE [Products] SET [Price]=90 WHERE [Name]=N'Картошка';
+
+SELECT COUNT(*) AS Count FROM [Products]
+
+DELETE FROM [Products] WHERE [Price]>1000;
